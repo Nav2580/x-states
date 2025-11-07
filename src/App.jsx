801bfe2af9ee624a181d8625ea755d04f4c8b16d
Nav2580 <nk6211633@gmail.com>
2025-11-07 20:12:11 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
+import React from "react";
+import Main from "./components/Main";
 
-import './App.css';
-import Main from './Main';
 function App() {
   return (
-    <div className="App">
-      <Main/>
+    <div>
+      <Main />
     </div>
   );
 }
